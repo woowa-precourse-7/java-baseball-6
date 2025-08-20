6f43c4c0c4584002c1aeda3d9794d7c893b127ea
bee9827 <ehfrhfo9494@naver.com>
2025-08-20 22:49:36 +0900

    feat: BaseballOutputView

---
+++ src/main/java/baseball/view/BaseballOutputView.java
@@ -0,0 +1,36 @@
+package baseball.view;
+
+import baseball.model.BaseballState;
+import java.util.Map;
+
+public class BaseballOutputView implements OutputView {
+    @Override
+    public void printInstruction() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    @Override
+    public void printState(Map<BaseballState, Integer> states) {
+        if (states.isEmpty()) {
+            System.out.println(BaseballState.NOTHING.getInfo());
+            return;
+        }
+
+        for (BaseballState state : BaseballState.values()) {
+            printIfContains(states, state);
+        }
+
+        System.out.println();
+    }
+
+    private void printIfContains(Map<BaseballState, Integer> states, BaseballState state) {
+        if (states.containsKey(state)) {
+            System.out.printf("%d%s ", states.get(state), state.getInfo());
+        }
+    }
+
+    @Override
+    public void printGameEndInstruction() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+}
