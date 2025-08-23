7fb7f26404ac614d4c2823710bd562c79ca2e19d
DEV_KANGSAN <devstar1223@gmail.com>
2025-08-24 00:42:00 +0900

    refactor : 잘못된 게임 시작/종료 값 예외 처리

--- src/main/java/baseball/game/GameStatus.java
+++ src/main/java/baseball/game/GameStatus.java
@@ -16,6 +16,7 @@ public enum GameStatus {
                 return gameStatus;
             }
         }
-        return null;
+
+        throw new IllegalArgumentException("잘못된 입력입니다. 게임 재시작은 1, 종료는 2만 입력할 수 있습니다.");
     }
 }
